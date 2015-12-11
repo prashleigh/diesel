4f0cf3cbf15b65563aa49c5af3ec5d079504e2b4
Patrick Rashleigh <edward_rashleigh@brown.edu>
2015-12-11 14:42:18 -0500

    Remove logging code

--- scripts/timesheets-main.js
+++ scripts/timesheets-main.js
@@ -4,8 +4,8 @@ define(['timesheets'], function(ts) {
   'use strict';
   
   console.log('* Timesheets.js loaded'); // TEMP
-  console.log('DOM at time of timesheets: ');
-  console.log(document.getElementById('slideshow').innerHTML);
+  // console.log('DOM at time of timesheets: ');
+  // console.log(document.getElementById('slideshow').innerHTML);
   require(['timesheets-controls', 'timesheets-navigation'], function () {
     console.log('* Timesheets-controls and -navigation loaded'); // TEMP
   });
